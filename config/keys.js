0c4992a209ac507b20599c05630db9c570bc2f2c
Мельник Олександр Вячеславович <MelnikOV@fib.com>
2019-05-16 10:55:01 +0300

    deploy test

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI: 
-  'mongodb+srv://netcat:harrier@cluster0-kdkkw.mongodb.net/test?retryWrites=true'
+  'mongodb+srv://netcat:harrier@cluster0-kdkkw.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'secret'
 };
 
